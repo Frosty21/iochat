7b2a1dd19b4c7432c72e20292b53f3712c921c73
Frosty21 <nathan.froese21@gmail.com>
2017-04-21 15:15:32 -0700

    set index with messages

--- server.js
+++ server.js
@@ -11,3 +11,12 @@ console.log('Server Running...');
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html')
 });
+
+io.sockets.on('connection',(sockets) => {
+  connections.push(socket);
+  console.log('connected: %s sockets connected', connections.length);
+
+// disconnect
+  connections.splice(connections.indexOf(socket),1);
+  console.log('Dissconnected: %s sockets connected', connections.length);
+});
